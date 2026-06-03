65b0cbd972febdef6f2ca9bbae882769fcd82a4f
Anshuman <anshumanpachauri26@gmail.com>
2026-06-03 12:00:00 +0530

    Created URL data model.

---
+++ src/main/java/com/ap/minify/models/UrlData.java
@@ -0,0 +1,23 @@
+package com.ap.minify.models;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UrlData {
+
+	private String originalUrl;
+	private String shortCode;
+	private LocalDateTime createdAt;
+	private LocalDateTime expiresAt;
+	private int clickCount;
+	private String createdBy;
+	private boolean isActive;
+}
